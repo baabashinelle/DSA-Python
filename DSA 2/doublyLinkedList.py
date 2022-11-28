5b131dbd35b326f02c06bdf06afc892beeb98ee0
Baaba <baabadampare@gmail.com>
2022-11-28 20:02:50 +0000

    update doubly linked list

--- DSA 2/doublyLinkedList.py
+++ DSA 2/doublyLinkedList.py
@@ -31,3 +31,35 @@ class DoublyLL:
             while n is not None:
                 print(n.data, "-->",end=" ")
                 n = n.pref
+
+    #inserting into an empty doubly linked list
+    def insert_empty(self,data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+        else:
+            print("linked list is not empty")
+
+    
+    
+    #insertion of node at the beginning of doubly linked list
+    def add_begin(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else :
+            new_node.nref = self.head
+            self.head.pref = new_node
+            self.head = new_node
+
+    #insertion of node at the end of doubly linked list
+    def add_begin(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            n.nref = new_node
+            new_node.pref = n
