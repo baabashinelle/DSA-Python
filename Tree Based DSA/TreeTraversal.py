9f28f60483a6387a942b016f0cb4c5ff8e85467f
Baaba <baabadampare@gmail.com>
2022-11-29 20:25:55 +0000

    tree traversal

---
+++ Tree Based DSA/TreeTraversal.py
@@ -0,0 +1,53 @@
+# Tree traversal in Python
+
+class Node:
+    def __init__(self,item):
+        self.right = None
+        self.left = None
+        self.val = item
+
+def inorder(root):
+
+    if root:
+        #traverse left
+        inorder(root.left)
+        #traverse root
+        print(str(root.val) + "->", end="")
+        #traverse right
+        inorder(root.right)
+
+def postorder(root):
+
+    if root:
+        # Traverse left
+        postorder(root.left)
+        # Traverse right
+        postorder(root.right)
+        # Traverse root
+        print(str(root.val) + "->", end='')
+
+
+def preorder(root):
+
+    if root:
+        # Traverse root
+        print(str(root.val) + "->", end='')
+        # Traverse left
+        preorder(root.left)
+        # Traverse right
+        preorder(root.right)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Inorder traversal ")
+inorder(root)
+
+print("\nPreorder traversal ")
+preorder(root)
+
+print("\nPostorder traversal ")
+postorder(root)
