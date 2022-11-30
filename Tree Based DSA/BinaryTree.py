83f3182ebacfbbfa4cf5832872f93394f1adeaf1
Baaba <baabadampare@gmail.com>
2022-11-30 00:36:58 +0000

    updates

--- Tree Based DSA/BinaryTree.py
+++ Tree Based DSA/BinaryTree.py
@@ -4,7 +4,7 @@ class Node:
     def __init__(self, key):
         self.left = None
         self.right = None
-        self.val = key
+        self.val = key 
 
     # Traverse preorder
     def traversePreOrder(self):
