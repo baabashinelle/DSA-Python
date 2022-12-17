b5b494f2bbe6c47a196c61b1f078447d15d964c1
Baaba <baabadampare@gmail.com>
2022-12-17 17:19:22 +0000

    valid anagram soln

--- DSA 2/dictionary.py
+++ DSA 2/dictionary.py
@@ -98,3 +98,23 @@ def anagram(s, t):
     
 print(anagram('eat', 'ate'))
 print(anagram('bike', 'book'))
+
+
+# another solution for valid anagram
+class Solution(object):
+    def isAnagram(self, s, t):
+        countS, countT = {},{}
+        # checking the length
+        if len(s) != len(t):
+            return False
+        
+        # counting the occurrences of letters in string s
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+            
+        return True
