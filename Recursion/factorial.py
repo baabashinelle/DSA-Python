623bbe0b7f5ec1cb24f2c1a2d6f944d6f8e64733
Baaba <baabadampare@gmail.com>
2023-02-01 13:51:02 +0000

    recursion

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -3,3 +3,11 @@ def factorial(number):
         return 1
     else:
         return number * factorial(number - 1) #factorial(number - 1) is the subproblem
+
+
+
+# Bottom up strategy using the trick of passing extra parameters
+# def factorial(n, i=1, product=1):
+#     return product if i > n
+#     return factorial(n, i + 1, product * i)
+# end
